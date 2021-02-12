18778812e7fc61208f7fde6d02cd9df2150ca7cf
karrelo <kardo.rahim@hotmail.se>
2021-02-12 10:21:39 +0100

    LandingPageLinks

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import '../components/Button.css';
+import Login from '../components/pages/Login'
+import {Link} from 'react-router-dom';
 
 export default class Button extends Component {
     constructor(props){
         super(props)
-        this.state = {myVar: props.myVar}
+        this.state = {myVar: props.myVar, myLink: ''}
+            
     }
 
-    // handleClick = (e) => {
-      
-    // }
-    render() {
 
-        return (
-            <button >{this.state.myVar}</button>
-        )
+    render() {
+        if(this.state.myVar === 'Login'){
+           return <button><Link to='/login' className="btn btn-primary">{this.state.myVar}</Link></button>
+        } else if(this.state.myVar === 'Register') {
+           return <button><Link to='/register' className="btn btn-primary">{this.state.myVar}</Link></button>
+        }
+
+       
     }
 }
