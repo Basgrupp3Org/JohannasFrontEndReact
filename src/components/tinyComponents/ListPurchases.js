7ae6339ad94f088ef1ce0b4c750d79001fce4f49
Conny Johansson <conjoh94@outlook.com>
2021-03-31 15:33:51 +0200

    Added category to purchase

--- src/components/tinyComponents/ListPurchases.js
+++ src/components/tinyComponents/ListPurchases.js
@@ -164,7 +164,7 @@ export default function ListPurchases() {
               <div key={key}>
 
                 {
-                  <Purchase PurchaseName={data.PurchaseName} Price={data.Price} Date={data.Date} Category={data.Category.Name} />
+                  <Purchase PurchaseName={data.PurchaseName} Price={data.Price} Date={data.Date} Category={data.Category} />
 
                 }
               </div>
