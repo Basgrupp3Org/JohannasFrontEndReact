6c24010875ea6c7dc031ea91dda4f02a1f4ef3e9
Simon <simonoskarkarvonen@gmail.com>
2021-04-28 10:30:19 +0200

    Fixat regex Purchase

--- src/components/modals/RegisterPurchaseModal.js
+++ src/components/modals/RegisterPurchaseModal.js
@@ -1,200 +1,204 @@
-import '../styling/RegisterPurchaseModal.css';
-import React, { useState, useEffect, useContext } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import { Input } from '@material-ui/core';
-import { UserContext } from '../contexts/UserContext';
+import "../styling/RegisterPurchaseModal.css";
+import React, { useState, useEffect, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Input } from "@material-ui/core";
+import { UserContext } from "../contexts/UserContext";
 
 function getModalStyle() {
-    const top = 50;
-    const left = 50;
+  const top = 50;
+  const left = 50;
 
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
 }
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: 'absolute',
-        width: 800,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-
-    },
+  paper: {
+    position: "absolute",
+    width: 800,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
 
 function RegisterPurchaseModal() {
-    const classes = useStyles();
-    const [modalStyle] = React.useState(getModalStyle);
-    const [summary, setSummary] = useState('');
-    const user = useContext(UserContext)
-    const [open, setOpen] = useState(false);
-    const [price, setPrice] = useState('');
-    const [purchaseName, setPurchaseName] = useState('');
-    const [date, setDate] = useState('');
-    const [budgets, setBudgets] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState('');
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [summary, setSummary] = useState("");
+  const user = useContext(UserContext);
+  const [open, setOpen] = useState(false);
+  const [price, setPrice] = useState("");
+  const [purchaseName, setPurchaseName] = useState("");
+  const [date, setDate] = useState("");
+  const [budgets, setBudgets] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
+  useEffect(() => {
+    fetch("http://localhost:65424/api/Budget/GetBudgetList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setBudgets(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
+  const handlePurchase = (event) => {
+    event.preventDefault();
+    console.log(selectedCategory);
+    let requestObject = {
+      Price: price,
+      PurchaseName: purchaseName,
+      Date: date,
+      User: {
+        Username: user,
+      },
+      Category: {
+        Id: selectedCategory,
+      },
+    };
 
-    useEffect(() => {
+    fetch("http://localhost:65424/api/Purchase/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestObject),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-        fetch('http://localhost:65424/api/Budget/GetBudgetList', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-            .then(data => data.json())
-            .then(data => { setBudgets(data) })
-            .catch((err) => {
-                console.error(err);
-            })
+  const handleBudgetChange = (e) => {
+    e.preventDefault();
 
+    let requestObject = {
+      Price: price,
+      PurchaseName: purchaseName,
+      Date: date,
+      User: {
+        Username: user,
+      },
+      Budget: {
+        Id: e.target.value,
+      },
+    };
 
+    fetch("http://localhost:65424/api/Category/GetCategoryForPurchase", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestObject),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
-
-
-
-    }, [])
-
-    const handlePurchase = (event) => {
-        event.preventDefault();
-        console.log(selectedCategory)
-        let requestObject = {
-
-            Price: price,
-            PurchaseName: purchaseName,
-            Date: date,
-            User: {
-                Username: user,
-            },
-            Category: {
-                Id: selectedCategory
-            }
-        }
-
-        fetch('http://localhost:65424/api/Purchase/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestObject)
-        })
-            .then(data => data.json())
-            .then(data => { console.log(data) })
-            .catch((err) => {
-                console.error(err);
-            })
+  const setPurchaseName1 = (e) => {
+    if (purchaseRegex.exec(e.target.value)) {
+      setPurchaseName(e.target.value);
+    } else {
+      setSummary("no numbers allowed");
     }
+  };
 
-    const handleBudgetChange = (e) => {
-        e.preventDefault();
+  const purchaseRegex = new RegExp("^[a-zåäöé]{0,19}$");
 
+  return (
+    <div className="rpm">
+      <button onClick={() => setOpen(true)}>Register Purchase</button>
 
-        let requestObject = {
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="rpm__registerpurchase">
+            <center>
+              <h2 className="rpm__purchasetext">Register Purchase</h2>
+            </center>
+            {/* {purchaseName} */}
+            {summary}
+            <input
+              type="text"
+              name="purchaseName"
+              // validations={{matchRegexp:purchaseRegex}}
+              // validationErrors={{matchRegexp:"Hej"}}
+              pattern="^[a-zåäöé]{0,19}$"
+              placeholder="Purchase Name"
+              value={purchaseName}
+              // onChange={(e) => setPurchaseName(e.target.value)}
+              onChange={setPurchaseName1}
+              required
+            ></input>
 
-            Price: price,
-            PurchaseName: purchaseName,
-            Date: date,
-            User: {
-                Username: user,
-            },
-            Budget: {
-                Id: e.target.value
-            }
-        }
+            <Input
+              placeholder="Price"
+              type="number"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
 
-        fetch('http://localhost:65424/api/Category/GetCategoryForPurchase', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestObject)
-        })
-            .then(data => data.json())
-            .then(data => { setCategories(data) })
-            .catch((err) => {
-                console.error(err);
-            })
-    }
+            <Input
+              placeholder="Date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
 
-    const setPurchaseName1 = (e) => {
-        if(purchaseRegex.exec(e.target.value)){
-            setPurchaseName(e.target.value);
-        }else {
-            setSummary("no numbers allowed");
-        }
-    }
+            <select onChange={handleBudgetChange}>
+              <option selected disabled hidden>
+                --Choose Budget--
+              </option>
+              {budgets.map((x) => (
+                <option value={x.Id}>{x.BudgetName}</option>
+              ))}
+            </select>
+            <select onChange={(e) => setSelectedCategory(e.target.value)}>
+              <option selected disabled hidden>
+                --Choose Category--
+              </option>
+              {categories.length
+                ? categories.map((x) => <option value={x.Id}>{x.Name}</option>)
+                : undefined}
+            </select>
+            <input type="submit" value="ok"></input>
 
-    const purchaseRegex = new RegExp('[a-zåäöé]{0,19}')
-    
-    return (
-        <div className="rpm">
-            <button onClick={() => setOpen(true)}>Register Purchase</button>
-
-            <Modal
-                open={open}
-                onClose={() => setOpen(false)}>
-                <div style={modalStyle} className={classes.paper}>
-                    <form className="rpm__registerpurchase">
-                        <center>
-                            <h2 className="rpm__purchasetext">
-                                Register Purchase
-                            </h2>
-                        </center>
-                        {purchaseName}
-                        {summary}
-                        <input
-                            type="text"
-                            name="purchaseName"
-                            // validations={{matchRegexp:purchaseRegex}}
-                            // validationErrors={{matchRegexp:"Hej"}}
-                            pattern="[a-zåäöé]{0,19}"
-                            placeholder="Purchase Name"
-                            value={purchaseName}
-                            // onChange={(e) => setPurchaseName(e.target.value)} 
-                            onChange={setPurchaseName1} 
-                            required></input>
-                        
-
-                        <Input
-                            placeholder="Price"
-                            type="number"
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)} />
-
-                        <Input
-                            placeholder="Date"
-                            type="date"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)} />
-
-                    <select
-                            onChange={handleBudgetChange}>
-                            <option selected disabled hidden>--Choose Budget--</option>
-                            {budgets.map(x => <option value={x.Id}>{x.BudgetName}</option>)}
-                        </select>
-                        <select
-                            onChange={(e) => setSelectedCategory(e.target.value)}>
-                            <option selected disabled hidden>--Choose Category--</option>
-                            {categories.length ? categories.map(x => <option value={x.Id}>{x.Name}</option>) : undefined}
-                        </select>
-                        <input type="submit" value="ok"></input>
-
-                        <button variant="contained" className="rpm__submitpurchase" onClick={handlePurchase} disableElevation>Submit</button>
-                    </form>
-                </div>
-            </Modal>
+            <button
+              variant="contained"
+              className="rpm__submitpurchase"
+              onClick={handlePurchase}
+              disableElevation
+            >
+              Submit
+            </button>
+          </form>
         </div>
-    )
+      </Modal>
+    </div>
+  );
 }
 
-export default RegisterPurchaseModal
+export default RegisterPurchaseModal;
--- src/components/tinyComponents/ListPurchases.js
+++ src/components/tinyComponents/ListPurchases.js
@@ -1,171 +1,176 @@
-import React, { useContext, useState, useEffect } from 'react'
-import Navbar from './Navbar'
-import { UserContext } from '../contexts/UserContext'
-import Purchase from './Purchase'
-import { List } from '@material-ui/core';
-import '../styling/Purchase.css'
+import React, { useContext, useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import { UserContext } from "../contexts/UserContext";
+import Purchase from "./Purchase";
+import { List } from "@material-ui/core";
+import "../styling/Purchase.css";
 
 export default function ListPurchases() {
-
   const user = useContext(UserContext);
   const [purchases, setPurchases] = useState([]);
   const [dateFrom, setFromDate] = useState();
   const [dateTo, setToDate] = useState();
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
-    
     let requestObject = {
+      UserName: user,
+    };
 
-       
-        "UserName": user
-
-     }
-    
-
-    fetch('http://localhost:65424/api/Purchase/GetPurchaseList', {
-      method: 'POST',
-      headers:
-      {
-        'Content-Type': 'application/json'
+    fetch("http://localhost:65424/api/Purchase/GetPurchaseList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(requestObject)
+      body: JSON.stringify(requestObject),
     })
-      .then(data => data.json())
-      .then(data => { setPurchases(data) })
+      .then((data) => data.json())
+      .then((data) => {
+        setPurchases(data);
+      })
       .catch((err) => {
         console.error(err);
-      })
+      });
   }, []);
 
-  // useEffect(() => {
-
-  // //   fetch('http://localhost:65424/api/Category/GetCategoryList', {
-  // //     method: 'POST',
-  // //     headers: {
-  // //       'Content-Type': 'application/json'
-  // //     },
-  // //     body: JSON.stringify(user)
-  // //   })
-  // //     .then(data => data.json())
-  // //     .then(data => { setCategories(data) })
-  // //     .catch((err) => {
-  // //       console.error(err);
-  // //     })
-  // // }, [])
+  useEffect(() => {
+    fetch("http://localhost:65424/api/Category/GetCategoryList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   const filterPurchases = (e) => {
-
-    e.preventDefault()
+    e.preventDefault();
 
     let requestObject = {
-
-      "FromDate": dateFrom,
-      "ToDate": dateTo,
-      "User": {
-        "Username": user
-
-      }
-    }
-
-    fetch('http://localhost:65424/api/Purchase/GetPurchaseListByDate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
+      FromDate: dateFrom,
+      ToDate: dateTo,
+      User: {
+        Username: user,
       },
-      body: JSON.stringify(requestObject)
+    };
+
+    fetch("http://localhost:65424/api/Purchase/GetPurchaseListByDate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestObject),
     })
-      .then(data => data.json())
-      .then(data => { setPurchases(data) })
+      .then((data) => data.json())
+      .then((data) => {
+        setPurchases(data);
+      })
       .catch((err) => {
         console.error(err);
-      })
+      });
 
-    console.log(requestObject)
-  }
-
+    console.log(requestObject);
+  };
 
   const filterCategories = (e) => {
-
-    e.preventDefault()
+    e.preventDefault();
 
     let requestObject = {
-
-      "User": {
-        "Username": user
-
+      User: {
+        Username: user,
       },
-      "Category": {
-        "Id": selectedCategory
-      }
+      Category: {
+        Id: selectedCategory,
+      },
+    };
 
-    }
-
-    fetch('http://localhost:65424/api/Purchase/GetPurchaseListByCategory', {
-      method: 'POST',
+    fetch("http://localhost:65424/api/Purchase/GetPurchaseListByCategory", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(requestObject)
+      body: JSON.stringify(requestObject),
     })
-      .then(data => data.json())
-      .then(data => { setPurchases(data) })
+      .then((data) => data.json())
+      .then((data) => {
+        setPurchases(data);
+      })
       .catch((err) => {
         console.error(err);
-      })
+      });
 
-    console.log(requestObject)
-  }
+    console.log(requestObject);
+  };
 
   return (
     <div>
-
       <>
         <div className="Filtering">
           <label className="fromDateLabel">From Date</label>
-          <input className="fromDate"
-
+          <input
+            className="fromDate"
             placeholder="From Date"
             type="date"
             value={dateFrom}
-            onChange={(e) => setFromDate(e.target.value)} />
+            onChange={(e) => setFromDate(e.target.value)}
+          />
           <label className="toDateLabel">To Date</label>
           <input
             className="toDate"
             placeholder="From Date"
             type="date"
             value={dateTo}
-            onChange={(e) => setToDate(e.target.value)} />
+            onChange={(e) => setToDate(e.target.value)}
+          />
 
-          <button className="dateButton" onClick={filterPurchases}>Filter Date</button>
+          <button className="dateButton" onClick={filterPurchases}>
+            Filter Date
+          </button>
         </div>
         <div className="Filtering">
           <label className="CategoryLabel">Choose Category</label>
-          <select className="categoryInput"
-            onChange={(e) => setSelectedCategory(e.target.value)}>
-            <option selected disabled hidden>--empty--</option>
-            {categories.length ? categories.map(x => <option value={x.Id}>{x.Name}</option>) : undefined}
+          <select
+            className="categoryInput"
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            <option selected disabled hidden>
+              --empty--
+            </option>
+            {categories.length
+              ? categories.map((x) => <option value={x.Id}>{x.Name}</option>)
+              : undefined}
           </select>
-          <button className="categoryButton" onClick={filterCategories}>Filter Category</button>
-
+          <button className="categoryButton" onClick={filterCategories}>
+            Filter Category
+          </button>
         </div>
         <div className="purchase-container">
-
-
-          {purchases.length ? purchases.map((data, key) => {
-            return (
-              <div key={key}>
-
-                {
-                  <Purchase PurchaseName={data.PurchaseName} Price={data.Price} Date={data.Date} Category={data.Category} />
-
-                }
-              </div>
-            );
-          }) : undefined}
+          {purchases.length
+            ? purchases.map((data, key) => {
+                return (
+                  <div key={key}>
+                    {
+                      <Purchase
+                        PurchaseName={data.PurchaseName}
+                        Price={data.Price}
+                        Date={data.Date}
+                        Category={data.Category}
+                      />
+                    }
+                  </div>
+                );
+              })
+            : undefined}
         </div>
       </>
-    </div >
-  )
+    </div>
+  );
 }
